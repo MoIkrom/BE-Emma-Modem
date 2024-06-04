4543ed801364a996d2ade04d2ce4de0a1e860acb
moikrom <mikram2162gmail.com>
2024-06-04 14:58:29 +0700

    req

--- src/routes/rt_inbox.js
+++ src/routes/rt_inbox.js
@@ -10,7 +10,7 @@ Router.get("/webhook", async (req, res) => {
 
     res.status(200).json({
       msg: "Success Get Data",
-      data: req.body,
+      data: req,
     });
   } catch (error) {
     console.error("Error processing webhook:", error);
